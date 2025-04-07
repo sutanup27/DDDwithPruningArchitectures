9a5c83a2d2519a728128772fc7f4c7c59e60f7f5
sutanup27 <sutanupaul27@gmail.com>
2025-04-07 22:13:00 +0530

    Updated with FGP and CP codes

--- PruneTrain.py
+++ PruneTrain.py
@@ -5,9 +5,9 @@ import torch
 from DataPreprocessing import get_dataloaders
 from Model_Evaluation import get_model_size, get_sparsity
 from TrainingModules import TrainingPrunned, evaluate, train
-from Utill import FineGrainedPruner, plot_sensitivity_scan, sensitivity_scan
+from Utill import ChannelPrunner, FineGrainedPruner, plot_sensitivity_scan, print_model, sensitivity_scan
 from VGG import VGG
-from Viewer import plot_weight_distribution  # Ensure you import your correct model architecture
+from Viewer import plot_accuracy, plot_weight_distribution  # Ensure you import your correct model architecture
 seed=0
 random.seed(seed)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -18,37 +18,61 @@ MiB = 1024 * KiB
 GiB = 1024 * MiB
 # Initialize the model
 model = VGG()  # Replace with your actual model class
-path="./mrleyedataset"
+path="../mrleyedataset"
 #model_path='./checkpoint/vgg_mrl_99.51375579833984.pth'
-model_path='vgg_mrl_99.51.pth'
+model_path='vgg_mrl_99.09.pth'
 # Load the saved state_dict correctly
 state_dict = torch.load(model_path, map_location=torch.device(device))  # Use 'cpu' if necessary
 missing_keys, unexpected_keys = model.load_state_dict(state_dict, strict=False)
 
-sparsity_dict = {
-'backbone.conv0.weight': 0.5,
-'backbone.conv1.weight': 0.85,
-'backbone.conv2.weight': 0.85,
-'backbone.conv3.weight': 0.9,
+model.to(device)
+select_model='vgg'
+pruning_type='FGP'
+
+
+sparsity_dict = {      #for FGP
+'backbone.conv0.weight': 0.4,
+'backbone.conv1.weight': 0.90,
+'backbone.conv2.weight': 0.80,
+'backbone.conv3.weight': 0.6,
 'backbone.conv4.weight': 0.9,
 'backbone.conv5.weight': 0.9,
-'backbone.conv6.weight': 0.9,
+'backbone.conv6.weight': 0.8,
 'backbone.conv7.weight': 0.95,
 'backbone.conv8.weight': 0.95,
-'backbone.conv9.weight': 0.95,
+'backbone.conv9.weight': 0.97,
 'fc2.weight': 0.95,
 }
 
-train_dataloader,test_dataloader=get_dataloaders(path ) # Basemodel
+# sparsity_dict = {       #for CP
+# 'backbone.conv0.weight': 0.2,
+# 'backbone.conv1.weight': 0.7,
+# 'backbone.conv2.weight': 0.6,
+# 'backbone.conv3.weight': 0.5,
+# 'backbone.conv4.weight': 0.7,
+# 'backbone.conv5.weight': 0.9,
+# 'backbone.conv6.weight': 0.6,
+# 'backbone.conv7.weight': 0.7,
+# 'backbone.conv8.weight': 0.8,
+# 'backbone.conv9.weight': 0.9,
+# 'fc2.weight': 0.95,
+# }
+
+train_dataloader,test_dataloader=get_dataloaders(path, batch_size=64 ) # Basemodel
 dense_model_accuracy=evaluate(model,test_dataloader)
-
-
+print('dense_model_accuracy:',dense_model_accuracy)
 pruned_model=copy.deepcopy(model)
+if pruning_type=='FGP':
+    isCallback=True
+    pruner = FineGrainedPruner(pruned_model, sparsity_dict)
+elif pruning_type=='CP':
+    pruned_model = ChannelPrunner(pruned_model, sparsity_dict)
+    pruner=None
+    isCallback=False
+else:
+    exit
 
-pruner = FineGrainedPruner(pruned_model, sparsity_dict)
-print(f'After pruning with sparsity dictionary')
-for name, sparsity in sparsity_dict.items():
-    print(f'  {name}: {sparsity:.2f}')
+print_model(pruned_model)
 print(f'The sparsity of each layer becomes')
 for name, param in pruned_model.named_parameters():
     if name in sparsity_dict:
@@ -61,10 +85,16 @@ print(f"Sparse model has size={sparse_model_size / MiB:.2f} MiB = {sparse_model_
 sparse_model_accuracy,_ = evaluate(pruned_model, test_dataloader)
 print(f"Sparse model has accuracy={sparse_model_accuracy:.2f}% before fintuning")
 
-num_finetune_epochs = 5
-optimizer = torch.optim.SGD(pruned_model.parameters(), lr=0.0001, momentum=0.9, weight_decay=1e-4)
+num_finetune_epochs = 20
+optimizer = torch.optim.SGD(pruned_model.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, num_finetune_epochs)
 criterion = nn.CrossEntropyLoss()
-pruner = FineGrainedPruner(pruned_model, sparsity_dict)
 
-pruned_model_accuracy,pruned_model=TrainingPrunned(pruned_model,train_dataloader,test_dataloader,criterion, optimizer, pruner,scheduler=None,num_finetune_epochs=5):
+pruned_model_accuracy,best_pruned_model,accuracies=TrainingPrunned(pruned_model,train_dataloader,test_dataloader,criterion, optimizer, pruner,scheduler=None,num_finetune_epochs=num_finetune_epochs,isCallback=isCallback)
+
+torch.save(best_pruned_model, f'./checkpoint/{select_model}_mrl_{pruning_type}_{pruned_model_accuracy}.pth')
+
+sparse_model_accuracy,_ = evaluate(best_pruned_model, test_dataloader)
+
+print(sparse_model_accuracy)
+plot_accuracy(accuracies)
--- Train.py
+++ Train.py
@@ -22,9 +22,9 @@ print("Device:",device)
 seed=0
 random.seed(seed)
 
-path='./mrleyedataset'
+path='../mrleyedataset'
 
-train_dataloader,test_dataloader=get_dataloaders(path,train_transform, test_transform)
+train_dataloader,test_dataloader=get_dataloaders(path,train_transform, test_transform,batch_size=64)
 
 select_model='vgg'
 if select_model=='vgg':
@@ -42,12 +42,14 @@ model = model.to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = SGD( model.parameters(), lr=0.001,  momentum=0.9,  weight_decay=5e-4,)
 
-lambda_lr = lambda epoch: math.sqrt(.1) ** (epoch // 7)
-#lambda_lr = lambda epoch: 0.1 ** (epoch // 5)
-scheduler=LambdaLR(optimizer,lambda_lr)
+# lambda_lr = lambda epoch: math.sqrt(.1) ** (epoch // 7)
+# lambda_lr = lambda epoch: 0.1 ** (epoch // 5)
+# scheduler=LambdaLR(optimizer,lambda_lr)
+num_epochs=20
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, num_epochs)
 # scheduler = CosineAnnealingLR(optimizer, T_max=50)
 
-best_model, losses, test_losses, accs=Training( model, train_dataloader, test_dataloader, criterion, optimizer, num_epochs=2,scheduler=scheduler)
+best_model, losses, test_losses, accs=Training( model, train_dataloader, test_dataloader, criterion, optimizer, num_epochs=num_epochs,scheduler=scheduler)
 
 model=copy.deepcopy(best_model)
 metric,_ = evaluate(model, test_dataloader)
@@ -56,7 +58,7 @@ print(f"Best model accuray:", metric)
 plot_accuracy(accs)
 plot_loss(losses,test_losses)
 
-torch.save(model.state_dict(), f'./checkpoint/{select_model}_mrl_{metric}.pth')
+torch.save(model, f'./checkpoint/{select_model}_mrl_{metric}.pth')
 
 
     
